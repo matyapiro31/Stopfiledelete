8c278678ef96628c35616d951511c842ece9bfd9
Akira Nakagawa <matyapiro31@gmail.com>
2016-02-26 19:07:30 +0900

    Save 1.

--- app/src/main/java/net/surlinter/akira/stopfiledelete/MainActivity.java
+++ app/src/main/java/net/surlinter/akira/stopfiledelete/MainActivity.java
@@ -1,5 +1,6 @@
 package net.surlinter.akira.stopfiledelete;
 
+import android.app.Activity;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -7,15 +8,21 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.common.api.GoogleApiClient;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.lang.Runtime;
 import java.lang.Process;
 
@@ -26,23 +33,43 @@ public class MainActivity extends AppCompatActivity {
      * See https://g.co/AppIndexing/AndroidStudio for more information.
      */
     private GoogleApiClient client;
+    //mode of the app.
+    protected int mode=0;
+    protected static final int usual = 1;
+    protected static final int help = 2;
+    public String exec_log="";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //init
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        addAttr(Environment.getExternalStorageDirectory().getPath() + "/test");
-        addAttr("/sdcard/test");
+        //Test
+        String test = "/storage/emulated/legacy/test";
+        addAttr(test);
+        final TextView TV1= (TextView)findViewById(R.id.tv1);
+        TV1.setText(test);
+
+        //set fab events
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                if (mode == help) {
+                    Snackbar.make(view,getString(R.string.search_message), Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                } else {
+                    java.util.Date date= new java.util.Date();
+                    exec_log += "Search fab was clicked. (" + date.toString() +")\n\n";
+                    Process pr_ls = execShell(new String[] {"su","-c","lsattr /storage/emulated/legacy/"});
+                    String output = getProcessStdOut(pr_ls);
+                    TV1.setText(output);
+                }
             }
         });
+
         // ATTENTION: This was auto-generated to implement the App Indexing API.
         // See https://g.co/AppIndexing/AndroidStudio for more information.
         client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
@@ -67,7 +94,6 @@ public class MainActivity extends AppCompatActivity {
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 
@@ -75,17 +101,17 @@ public class MainActivity extends AppCompatActivity {
         Runtime runtime = Runtime.getRuntime();
         Process process;
         String[] command = {
-                "su","-c","chattr","+i",file
+                "su","-c","chattr +i "+file
         };
 
         try {
             process = runtime.exec(command);
             process.waitFor();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
+        } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         }
+        java.util.Date date= new java.util.Date();
+        exec_log += "i flag was added to " + file + ". (" + date.toString() + ")\n\n";
 
         return true;
     }
@@ -94,19 +120,111 @@ public class MainActivity extends AppCompatActivity {
         Runtime runtime = Runtime.getRuntime();
         Process process;
         String[] command = {
-                "su","-c","chattr","+i",file
+                "su","-c","chattr","-i",file
         };
 
         try {
             process = runtime.exec(command);
             process.waitFor();
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+        java.util.Date date= new java.util.Date();
+        exec_log += "i flag was removed to " + file +". (" + date.toString() + ")\n\n";
+
+        return true;
+    }
+
+    public Process execShell(String[] cmdarray) {
+        Runtime runtime = Runtime.getRuntime();
+        Process process = null;
+        String output;
+        try {
+            process = runtime.exec(cmdarray);
         } catch (IOException e) {
             e.printStackTrace();
-        } catch (InterruptedException e) {
+        }
+        java.util.Date date= new java.util.Date();
+        exec_log += "shell command `" + cmdarray[0] + "` was executed. (" + date.toString() + ")\n";
+        exec_log += "arguments are";
+        exec_log += " \'";
+        for (String arg:cmdarray
+             ) {
+            exec_log += arg==cmdarray[0]?"":" " + arg;
+        } exec_log += "\'\n";
+        exec_log += "process is " + process.toString() +"\n\n";
+
+        return process;
+    }
+
+    public String getProcessStdOut(java.lang.Process pr) {
+        String str_stdout="";
+        try {
+            String line;
+            BufferedReader read_stdout = new BufferedReader(new InputStreamReader(pr.getInputStream()));
+
+            while ((line=read_stdout.readLine())!=null) {
+                str_stdout +=line + "\n";
+            }
+            read_stdout.close();
+        } catch (IOException e) {
             e.printStackTrace();
+        }
+        return str_stdout;
+    }
+
+    public String getProcessStdErr(java.lang.Process pr) {
+        String str_stderr="";
+        try {
+            String line;
+            BufferedReader read_stderr = new BufferedReader(new InputStreamReader(pr.getErrorStream()));
+
+            while ((line=read_stderr.readLine())!=null) {
+                str_stderr +=line + "\n";
+            }
+            read_stderr.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return str_stderr;
+    }
+
+    public boolean setProcessStdIn(java.lang.Process pr,String[] text) {
+        try {
+            DataOutputStream d_out = new DataOutputStream(pr.getOutputStream());
+            for (String word:text
+                 ) {
+                d_out.writeBytes(word+"\n");
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+        java.util.Date date= new java.util.Date();
+        exec_log += "Standard input for " + pr.toString() + " received text. (" + date.toString() + ")\n";
+        for (String word:text
+             ) {
+            exec_log += word + "\n";
         }
 
         return true;
+    }
+
+    public int exitProcess(java.lang.Process pr) {
+        int ret;
+        try {
+            pr.waitFor();
+            ret = pr.exitValue();
+            if (ret == -1) {
+                ret = 0x10;
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            pr.destroy();
+            ret = -1;
+        }
+        return ret;
     }
 
     @Override
@@ -122,9 +240,9 @@ public class MainActivity extends AppCompatActivity {
                 // TODO: If you have web page content that matches this app activity's content,
                 // make sure this auto-generated web page URL is correct.
                 // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
+                Uri.parse("http://android.surlinter.net/api"),
                 // TODO: Make sure this auto-generated app deep link URI is correct.
-                Uri.parse("android-app://net.surlinter.akira.stopfiledelete/http/host/path")
+                Uri.parse("android-app://net.surlinter.akira.stopfiledelete/http/android.surlinter.net/api")
         );
         AppIndex.AppIndexApi.start(client, viewAction);
     }
@@ -141,9 +259,9 @@ public class MainActivity extends AppCompatActivity {
                 // TODO: If you have web page content that matches this app activity's content,
                 // make sure this auto-generated web page URL is correct.
                 // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
+                Uri.parse("http://android.surlinter.net/api"),
                 // TODO: Make sure this auto-generated app deep link URI is correct.
-                Uri.parse("android-app://net.surlinter.akira.stopfiledelete/http/host/path")
+                Uri.parse("android-app://net.surlinter.akira.stopfiledelete/http/android.surlinter.net/api")
         );
         AppIndex.AppIndexApi.end(client, viewAction);
         client.disconnect();
